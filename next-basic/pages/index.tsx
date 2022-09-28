d593c0c118df735b6900fdde200d1a9578f037f7
Ardanrestun <ardanrn67@gmail.com>
2022-09-28 21:55:40 +0700

    day2

--- next-basic/pages/index.tsx
+++ next-basic/pages/index.tsx
@@ -1,8 +1,5 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import Footer from "../components/footer/footer";
-import Header from "../components/header/header";
+
 import Layout from "../components/layout";
 
 const Home: NextPage = () => {
--- next-basic/pages/users/[id].tsx
+++ next-basic/pages/users/[id].tsx
@@ -29,58 +29,59 @@ export default function DetailUser(props: UserDetail) {
   console.log({ user });
   return (
     <Layout title="Detail User">
-      <div className={styles.container}>
-        <img src={user.image} className={styles.detailimg} />
-        <div>
-          <h3 className={styles.titledetail}>
-            Detail Fake User {user.maidenName}
-          </h3>
+      <div className={styles.columnid}>
+        <div className={styles.cardid}>
+          <img
+            src={user.image}
+            alt={user.maidenName}
+            className={styles.imgid}
+          />
           <hr />
-          <table>
+          <table className={styles.table}>
             <tbody>
               <tr>
                 <td className={styles.cellq}>First Name</td>
-                <td className={styles.cellw}>:</td>
+                <td className={styles.celle}>:</td>
                 <td className={styles.cellw}>{user.firstName}</td>
               </tr>
               <tr>
                 <td className={styles.cellq}>Last Name</td>
-                <td className={styles.cellw}>:</td>
+                <td className={styles.celle}>:</td>
                 <td className={styles.cellw}>{user.lastName}</td>
               </tr>
               <tr>
                 <td className={styles.cellq}>Maiden Name</td>
-                <td className={styles.cellw}>:</td>
+                <td className={styles.celle}>:</td>
                 <td className={styles.cellw}>{user.maidenName}</td>
               </tr>
               <tr>
                 <td className={styles.cellq}>Birth Date</td>
-                <td className={styles.cellw}>:</td>
+                <td className={styles.celle}>:</td>
                 <td className={styles.cellw}>{user.birthDate}</td>
               </tr>
               <tr>
                 <td className={styles.cellq}>Address</td>
-                <td className={styles.cellw}>:</td>
+                <td className={styles.celle}>:</td>
                 <td className={styles.cellw}>{user.address.address}</td>
               </tr>
               <tr>
                 <td className={styles.cellq}>City</td>
-                <td className={styles.cellw}>:</td>
+                <td className={styles.celle}>:</td>
                 <td className={styles.cellw}>{user.address.city}</td>
               </tr>
               <tr>
                 <td className={styles.cellq}>Postal Code</td>
-                <td className={styles.cellw}>:</td>
+                <td className={styles.celle}>:</td>
                 <td className={styles.cellw}>{user.address.postalCode}</td>
               </tr>
               <tr>
                 <td className={styles.cellq}>State</td>
-                <td className={styles.cellw}>:</td>
+                <td className={styles.celle}>:</td>
                 <td className={styles.cellw}>{user.address.state}</td>
               </tr>
               <tr>
                 <td className={styles.cellq}>Email</td>
-                <td className={styles.cellw}>:</td>
+                <td className={styles.celle}>:</td>
                 <td className={styles.cellw}>{user.email}</td>
               </tr>
             </tbody>
